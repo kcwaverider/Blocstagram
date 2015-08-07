d5f03af1a913e162be408cae312cd0a946e8058c
Chad Clayton <chad@kcwaverider.com>
2015-08-07 08:22:36 -0500

    Finished Checkpoint & Homework

--- Blocstagram/Blocstagram/AppDelegate.m
+++ Blocstagram/Blocstagram/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "ViewController.h"
+#import "ImagesTableViewController.h"
 
 @interface AppDelegate ()
 
@@ -18,9 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[ImagesTableViewController alloc] init]];
+
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
-    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[ViewController new]];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
---
+++ Blocstagram/Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Chad Clayton on 8/6/15.
+//  Copyright (c) 2015 Chad Clayton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
