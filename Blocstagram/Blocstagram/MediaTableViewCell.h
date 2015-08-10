16c75d9b7e8ad6ad7a8a6e4a78c570ea4928e68e
Chad Clayton <chad@kcwaverider.com>
2015-08-10 17:21:49 -0500

    Finished complex data cells checkpoint

---
+++ Blocstagram/Blocstagram/MediaTableViewCell.h
@@ -0,0 +1,18 @@
+//
+//  MediaTableViewCell.h
+//  Blocstagram
+//
+//  Created by Chad Clayton on 8/7/15.
+//  Copyright (c) 2015 Chad Clayton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class Media;
+
+@interface MediaTableViewCell : UITableViewCell
+
+@property (nonatomic, strong) Media *mediaItem;
+
++ (CGFloat) heightForMediaItem:(Media *)mediaItem width:(CGFloat)width;
+
+@end
