477520a5edf6a080667902fa2044c3a37d38299b
Chad Clayton <chad@kcwaverider.com>
2015-08-11 22:30:58 -0500

    Completed Deletion Assignment

--- Blocstagram/Blocstagram/DataSource.m
+++ Blocstagram/Blocstagram/DataSource.m
@@ -147,6 +147,7 @@
 - (void) deleteMediaItem:(Media *)item {
     NSMutableArray *mutableArrayWithKVO = [self mutableArrayValueForKey:@"mediaItems"];
     [mutableArrayWithKVO removeObject:item];
+    [mutableArrayWithKVO insertObject:item atIndex:0];
 }
 
 
